096f55f89431586e44029bd456104a3b880b4bf8
Rodrigo Andrade <rmandrade@stefanini.com>
2022-11-30 22:38:57 -0300

    QUIZ/UPDATE - Finalizado v1.0.0

--- 1-nocoes-basicas/quiz/lib/main.dart
+++ 1-nocoes-basicas/quiz/lib/main.dart
@@ -1,67 +1,202 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuizApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QuizApp extends StatefulWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  State<QuizApp> createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  final List<QuizModel> _quizes = [
+    QuizModel(question: 'System.out.println serve para?', anwsers: [
+      AwnserModel(value: 'Chamar a saida padrao do sistema.'),
+      AwnserModel(value: 'Redirecinar o buffer de dados para a saida padrao.'),
+      AwnserModel(value: 'Imprimir um fluxo de dados na linguagem java.'),
+      AwnserModel(
+        value: 'Todas as alteranativas estao corretas.',
+        isCorrect: true,
+      ),
+    ]),
+    QuizModel(question: 'Linus Torvalds desenvolve?', anwsers: [
+      AwnserModel(value: 'Kernel do Linux.', isCorrect: true),
+      AwnserModel(value: 'Kernel do Windows.'),
+      AwnserModel(value: 'Kernel do IOS.'),
+      AwnserModel(value: 'Kernel do Android.'),
+    ]),
+    QuizModel(question: 'Sprint é um termo usado em?', anwsers: [
+      AwnserModel(value: 'Engenharia de Software.'),
+      AwnserModel(value: 'Metodologias Ageis.'),
+      AwnserModel(value: 'No sistema SCRUM.', isCorrect: true),
+      AwnserModel(value: 'Termo em ingles para corrida.'),
+    ]),
+  ];
+
+  int points = 0;
+  int actualQuestion = 0;
+  AwnserModel? _radioAwnser;
+
+  void _awnser() {
+    if (_radioAwnser != null && _radioAwnser!.isCorrect) {
+      points++;
+    }
+  }
+
+  void _nextQuestion() {
+    _awnser();
+    setState(() {
+      if (actualQuestion < _quizes.length) {
+        actualQuestion++;
+        _resetRadioAwnser();
+      }
+    });
+  }
+
+  void _resetRadioAwnser() => _radioAwnser = null;
+
+  void _resetPoints() => points = 0;
+
+  void _resetActualQuestion() => actualQuestion = 0;
+
+  bool _disableNextButtton() => _radioAwnser == null;
+
+  void _restart() {
+    setState(() {
+      _resetRadioAwnser();
+      _resetPoints();
+      _resetActualQuestion();
+    });
+  }
+
+  bool _isOverQuestions() => actualQuestion == _quizes.length;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Quiz'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: _isOverQuestions()
+              ? FinishScreen(
+                  pontuacao: points,
+                  reiniciar: _restart,
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(top: 80.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10.0,
+                          vertical: 15.0,
+                        ),
+                        child: Text(
+                          _quizes[actualQuestion].question,
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: _quizes[actualQuestion]
+                            .anwsers!
+                            .map((e) => Row(
+                                  children: [
+                                    Radio(
+                                        value: e,
+                                        groupValue: _radioAwnser,
+                                        onChanged: (newValue) {
+                                          setState(() {
+                                            _radioAwnser = newValue;
+                                          });
+                                        }),
+                                    Text(e.value)
+                                  ],
+                                ))
+                            .toList(),
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ElevatedButton(
+                              onPressed:
+                                  _disableNextButtton() ? null : _nextQuestion,
+                              child: const Text('Responder'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class FinishScreen extends StatelessWidget {
+  final void Function() reiniciar;
+  final int pontuacao;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const FinishScreen({
+    required this.pontuacao,
+    required this.reiniciar,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/undraw_winners.png"),
+        const Text(
+          'Todas as perguntas foram respondidas.',
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Sua pontuacao final foi de $pontuacao.",
+            style: const TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: reiniciar,
+          child: const Text('Reiniciar'),
+        )
+      ],
     );
   }
+}
+
+class QuizModel {
+  String question;
+  List<AwnserModel>? anwsers;
+
+  QuizModel({required this.question, this.anwsers});
+}
+
+class AwnserModel {
+  String value;
+  bool isCorrect;
+
+  AwnserModel({required this.value, this.isCorrect = false});
 }
